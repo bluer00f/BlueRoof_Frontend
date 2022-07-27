ad009051a7ecdff757f5f0d029e74fe8c95472fc
Sae-byeol <toquf0424@naver.com>
2022-07-27 12:56:09 +0900

    feat: 나의 청약정보 View

--- src/pages/editSubscription/Content/Subscription.jsx
+++ src/pages/editSubscription/Content/Subscription.jsx
@@ -6,15 +6,25 @@ import { BlackText } from '../../../components/commons/Font'
 import Flex from '../../../components/commons/Flex'
 import { RoundInput } from '../../../components/commons/Inputs'
 const Subscription = () => {
-    const ranks=["1위","2위"];
-    const [specialRanking, setSpecialRanking]=useState("1위");
-    const [ranking, setRanking]=useState("1위");
+    const ranks=["1순위","2순위"];
+    const banks=["주택청약종저축", "청약저축", "청약예금", "청약부금"];
+
+    const [specialRanking, setSpecialRanking]=useState("1순위");
+    const [ranking, setRanking]=useState("1순위");
+    const [rankHistory, setRankHistory]=useState("1순위");
+    const [bank, setBank]=useState("주택청약종합저축");
 
     const ClickSpecialRank=(e)=>{
         setSpecialRanking(e.target.value);
     }
     const ClickRank=(e)=>{
         setRanking(e.target.value);
+    }
+    const ClickRankHistory=(e)=>{
+        setRankHistory(e.target.value);
+    }
+    const ClickBank=(e)=>{
+        setBank(e.target.value);
     }
   return (
       <SubscriptionContainer>
@@ -39,7 +49,7 @@ const Subscription = () => {
             <BlackText size="36px" weight="700">ㅣ 일반 공급 순위</BlackText>
             <a href="https://www.naver.com" style={{fontSize:"14px"}}>순위 확인 바로가기</a>
         </RowContainer>
-        <RadioBtns style={{marginBottom:"70px"}}>
+        <RadioBtns>
         {ranks.map((rank)=>(
                     <Label>
                         <RadioBtn
@@ -52,33 +62,54 @@ const Subscription = () => {
                     </Label>
                 ))}        
         </RadioBtns>
-        <BtnContainer>
-            <BlueRoundBtn>임시저장</BlueRoundBtn>
-        </BtnContainer>
-        <BlackText size="36px" weight="700" style={{marginTop:"50px"}}>ㅣ 2년 내 당첨 이력</BlackText>
+       <Line/>
+        <BlackText size="36px" weight="700">ㅣ 2년 내 당첨 이력</BlackText>
         <RadioBtns style={{marginBottom:"10px"}}>
         {ranks.map((rank)=>(
                     <Label>
                         <RadioBtn
                             type="radio"
                             value={rank}
-                            checked={ranking===rank}
-                            onChange={ClickRank}
+                            checked={rankHistory===rank}
+                            onChange={ClickRankHistory}
                         ></RadioBtn>
                         {rank}
                     </Label>
                 ))}        
         </RadioBtns>
         <ColContainer style={{gap:"20px", margin:"30px 0"}}>
-        <RoundInput width={"600px"} height={"50px"} placeholder="당첨일자"/>
-        <RoundInput width={"600px"} height={"50px"} placeholder="당첨지역"/>
-        <RowContainer style={{gap:"10px"}}>
-            <RoundInput width={"200px"} height={"50px"} placeholder="우편번호"></RoundInput>
-            <RoundInput width={"390px"} height={"50px"} placeholder="당첨아파트명"/>
-        </RowContainer>
-       
+            <RoundInput width={"80%"} height={"50px"} placeholder="당첨일자"/>
+            <RoundInput width={"80%"} height={"50px"} placeholder="당첨지역"/>
+            <RowContainer style={{gap:"10px", width: "80%"}}>
+                <RoundInput width={"20%"} height={"50px"} placeholder="우편번호"></RoundInput>
+                <RoundInput width={"80%"} height={"50px"} placeholder="당첨아파트명"/>
+            </RowContainer>
         </ColContainer>
-        
+        <Line/>
+        <BlackText size="36px" weight="700">ㅣ 청약 통장 종류</BlackText>
+        <RadioBtns>
+        {banks.map((b)=>(
+                    <Label style={{marginBottom:"20px"}}>
+                        <RadioBtn
+                            type="radio"
+                            value={b}
+                            checked={bank===b}
+                            onChange={ClickBank}
+                        ></RadioBtn>
+                        {b}
+                    </Label>
+                ))}        
+        </RadioBtns>
+        <RowContainer style={{gap:"10px",marginBottom:"50px"}}>
+            <BlackText size="36px" weight="700">ㅣ 청약 통장 가입일</BlackText>
+            <a href="https://www.naver.com" style={{fontSize:"14px"}}>가입일 확인 바로가기</a>
+        </RowContainer>
+        <ColContainer style={{marginBottom:"50px"}}>
+            <RoundInput width={"80%"} height={"50px"} placeholder="가입일자"/>
+        </ColContainer>
+        <BtnContainer>
+            <BlueRoundBtn>다음 &gt;</BlueRoundBtn>
+        </BtnContainer>
       </SubscriptionContainer>
    
   )
@@ -86,11 +117,12 @@ const Subscription = () => {
 
 export default Subscription
 const SubscriptionContainer=styled.div`
-    
+    padding-top: 50px;
 `
 const RadioBtns=styled(RowContainer)`
-    gap:10%;
-    margin: 50px 0 130px 10%;
+    margin: 50px 0 100px 10%;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
 `
 const RadioBtn=styled.input`
     appearance: none;
@@ -110,5 +142,10 @@ const Label=styled.label`
 const BtnContainer=styled(Flex)`
     justify-content: flex-end;
     padding-bottom: 30px;
-    border-bottom: 1px solid #B3B3B3;
+`
+const Line=styled.div`
+    width: 100%;
+    height:1px;
+    background-color: #B3B3B3;
+   margin: 100px 0;
 `
